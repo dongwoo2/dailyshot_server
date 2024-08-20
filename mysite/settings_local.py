394e02bc278ab5d05b01420e72dd0bc2a439f311
dongwoo2 <ieiedong@naver.com>
2024-08-20 21:41:57 +0900

    localsetting 수정

--- mysite/settings_local.py
+++ mysite/settings_local.py
@@ -1,5 +1,13 @@
 from .settings import *
 
+import environ
+
+env = environ.Env(DEBUG=(bool,True))
+
+#환경변수 파일 읽어오기
+environ.Env.read_env(
+        env_file=os.path.join(BASE_DIR, '.env')
+)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
