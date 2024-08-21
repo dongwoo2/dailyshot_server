0255c3e49ed7825fbabcfef748fabc209d3a03cc
dongwoo2 <ieiedong@naver.com>
2024-08-21 12:20:53 +0900

    local setting 수정

--- mysite/settings_local.py
+++ mysite/settings_local.py
@@ -4,6 +4,10 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': 'localhost',
+        'USER': env('MYSQL_USER_NAME',default='root'),
+        'PASSWORD': 'MYSQL_PASSWORD',
+        'PORT': '3306',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
