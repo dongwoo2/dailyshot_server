ec69f52bdb301ed88dfd068b97ddbd22e391a601
dongwoo2 <ieiedong@naver.com>
2024-08-21 12:09:48 +0900

    environ 모듈 추가

--- mysite/settings_local.py
+++ mysite/settings_local.py
@@ -1,16 +1,10 @@
 from .settings import *
 
-import environ
 
-env = environ.Env(DEBUG=(bool,True))
-
-#환경변수 파일 읽어오기
-environ.Env.read_env(
-        env_file=os.path.join(BASE_DIR, '.env')
-)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': 'localhost',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
