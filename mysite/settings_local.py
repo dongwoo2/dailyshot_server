7f89cd8204d058afcc19c73d9e1cb062ec594c91
dongwoo2 <ieiedong@naver.com>
2024-08-21 12:34:21 +0900

    local 다시 수정

--- mysite/settings_local.py
+++ mysite/settings_local.py
@@ -1,16 +1,17 @@
-from .settings import *
-
+import os
 import environ
 
+# BASE_DIR 정의
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# 환경 변수 설정
 env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, 'local.env'))
+env.read_env(os.path.join(os.path.dirname(__file__), 'local.env'))
 
-
-
+# 데이터베이스 설정
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'PORT': '3306',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # SQLite의 경우 NAME만 필요합니다.
     }
 }
