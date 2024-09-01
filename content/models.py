1cb34420eb22df3fcfec2c697e0c0c40c0f39846
dongwoo2 <ieiedong@naver.com>
2024-09-01 10:50:04 +0900

    술 등록 리다이렉트 수정

--- content/models.py
+++ content/models.py
@@ -6,3 +6,13 @@ class Like(models.Model):
     feed_id = models.IntegerField(default=0)
     email = models.EmailField(default='')
     is_like = models.BooleanField(default=True)
+
+class Reply(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    reply_text = models.TextField(default='')
+
+class Bookmark(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    is_marked = models.BooleanField(default=True)
